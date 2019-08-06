5cc4a7090c76c2ed2c9539755bfd70c4452f6aab
UbunGit <296019487@qq.com>
2019-08-06 23:17:01 +0800

    添加重复请求

--- 001/demo/src/main/java/com/ubungit/demo/Controller/HomeController.java
+++ 001/demo/src/main/java/com/ubungit/demo/Controller/HomeController.java
@@ -1,13 +1,23 @@
 package com.ubungit.demo.Controller;
 
+import com.ubungit.demo.ApiResult;
+import com.ubungit.demo.aop.NoRepeatSubmit;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HomeController{
 
     @RequestMapping("/test")
-    public String Index(){
-        return "ceshi";
+    @NoRepeatSubmit(lockTime = 30)
+    public ApiResult Index() {
+        try {
+            // 模拟业务场景
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return new ApiResult(200, "chenggong", null);
     }
 }
--- 001/demo/src/main/java/com/ubungit/demo/DemoApplication.java
+++ 001/demo/src/main/java/com/ubungit/demo/DemoApplication.java
@@ -2,12 +2,27 @@ package com.ubungit.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+		return new RestTemplate(factory);
+	}
+
+	@Bean
+	public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+		return factory;
 	}
 
 }
